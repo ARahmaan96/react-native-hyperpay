4227b1580ebd04f8b97a4ef723295e8fbf465562
arahman <a.rahman.khallaf@gmail.com>
2026-06-28 12:56:34 +0300

    chore: bump version to 3.0.6

--- ios/HyperPay.m
+++ ios/HyperPay.m
@@ -27,13 +27,15 @@ RCT_EXPORT_MODULE(HyperPay)
 }
 
 RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(setup:(NSDictionary*)options) {
-    _shopperResultURL = [options valueForKey:@"shopperResultURL"] ?: @"";
-    _merchantIdentifier = [options valueForKey:@"merchantIdentifier"] ?: @"";
-    _companyName = [options valueForKey:@"companyName"] ?: @"";
-    _countryCode = [options valueForKey:@"countryCode"] ?: @"";
-    _supportedNetworks = [options valueForKey:@"supportedNetworks"] ?: @[];
+    _shopperResultURL = [self stringFromValue:[options valueForKey:@"shopperResultURL"]] ?: @"";
+    _merchantIdentifier = [self stringFromValue:[options valueForKey:@"merchantIdentifier"]] ?: @"";
+    _companyName = [self stringFromValue:[options valueForKey:@"companyName"]] ?: @"";
+    _countryCode = [self stringFromValue:[options valueForKey:@"countryCode"]] ?: @"";
 
-    NSString *newMode = [options valueForKey:@"mode"];
+    id supportedNetworks = [options valueForKey:@"supportedNetworks"];
+    _supportedNetworks = [supportedNetworks isKindOfClass:[NSArray class]] ? supportedNetworks : @[];
+
+    NSString *newMode = [self stringFromValue:[options valueForKey:@"mode"]];
     if ([newMode isEqual:@"LiveMode"]) {
         _mode = @"LiveMode";
         _provider = [OPPPaymentProvider paymentProviderWithMode:OPPProviderModeLive];
@@ -96,34 +98,160 @@ RCT_EXPORT_METHOD(createPaymentTransaction:(NSDictionary*)options resolver:(RCTP
 
 RCT_EXPORT_METHOD(applePay:(NSDictionary*)params resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
     _checkoutID = [params valueForKey:@"checkoutID"];
-    if ([params valueForKey:@"companyName"]) {
-        _companyName = [params valueForKey:@"companyName"];
+    if (![_checkoutID isKindOfClass:[NSString class]] || _checkoutID.length == 0) {
+        reject(@"applePay", @"checkoutID is required", nil);
+        return;
     }
 
-    PKPaymentRequest *request = [OPPPaymentProvider paymentRequestWithMerchantIdentifier:_merchantIdentifier
-                                                                             countryCode:_countryCode];
-    if (_supportedNetworks.count > 0) {
-        request.supportedNetworks = _supportedNetworks;
-    } else {
-        request.supportedNetworks = @[PKPaymentNetworkVisa, PKPaymentNetworkMasterCard, PKPaymentNetworkAmex, PKPaymentNetworkDiscover];
+    NSString *companyName = [self stringFromValue:[params valueForKey:@"companyName"]];
+    if (companyName) {
+        _companyName = companyName;
     }
-    request.merchantCapabilities = PKMerchantCapability3DS;
-    request.currencyCode = [[NSLocale currentLocale] objectForKey:NSLocaleCurrencyCode] ?: @"USD";
-    NSDecimalNumber *amount = [NSDecimalNumber decimalNumberWithMantissa:[[params valueForKey:@"amount"] intValue] exponent:-2 isNegative:NO];
-    request.paymentSummaryItems = @[[PKPaymentSummaryItem summaryItemWithLabel:_companyName amount:amount]];
 
-    _applePayResolve = resolve;
-    _applePayReject = reject;
-    PKPaymentAuthorizationViewController *vc = [[PKPaymentAuthorizationViewController alloc] initWithPaymentRequest:request];
-    if (vc) {
-        vc.delegate = self;
-        dispatch_async(dispatch_get_main_queue(), ^{
-            UIViewController *rootViewController = UIApplication.sharedApplication.keyWindow.rootViewController;
-            [rootViewController presentViewController:vc animated:YES completion:nil];
-        });
-    } else {
-        reject(@"applePay", @"Apple Pay is not available", nil);
+    [_provider requestCheckoutDataWithCheckoutID:_checkoutID completionHandler:^(OPPCheckoutData * _Nullable checkoutData, NSError * _Nullable error) {
+        if (error) {
+            reject(@"applePay", error.localizedDescription ?: @"Unable to load Apple Pay checkout data", error);
+            return;
+        }
+
+        NSDecimalNumber *amount = [self decimalAmountFromValue:[params valueForKey:@"amount"]] ?: [self decimalAmountFromValue:checkoutData.amount];
+        if (!amount) {
+            reject(@"applePay", @"Apple Pay amount is required", nil);
+            return;
+        }
+
+        NSString *checkoutCurrency = [self stringFromValue:checkoutData.currency];
+        if (checkoutCurrency.length == 0) {
+            reject(@"applePay", @"Apple Pay currency is required", nil);
+            return;
+        }
+
+        [self presentApplePayWithAmount:amount currencyCode:checkoutCurrency resolver:resolve rejecter:reject];
+    }];
+}
+
+- (void)presentApplePayWithAmount:(NSDecimalNumber *)amount currencyCode:(NSString *)currencyCode resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
+    if (_merchantIdentifier.length == 0) {
+        reject(@"applePay", @"merchantIdentifier is required", nil);
+        return;
     }
+
+    if (_countryCode.length == 0) {
+        reject(@"applePay", @"countryCode is required", nil);
+        return;
+    }
+
+    if ([amount compare:[NSDecimalNumber zero]] != NSOrderedDescending) {
+        reject(@"applePay", @"Apple Pay amount must be greater than zero", nil);
+        return;
+    }
+
+    if (currencyCode.length == 0) {
+        reject(@"applePay", @"Apple Pay currency is required", nil);
+        return;
+    }
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        @try {
+            PKPaymentRequest *request = [OPPPaymentProvider paymentRequestWithMerchantIdentifier:_merchantIdentifier
+                                                                                     countryCode:_countryCode];
+            if (_supportedNetworks.count > 0) {
+                request.supportedNetworks = _supportedNetworks;
+            } else {
+                request.supportedNetworks = @[PKPaymentNetworkVisa, PKPaymentNetworkMasterCard, PKPaymentNetworkAmex, PKPaymentNetworkDiscover];
+            }
+            request.merchantCapabilities = PKMerchantCapability3DS;
+            request.currencyCode = currencyCode;
+            NSString *summaryLabel = _companyName.length > 0 ? _companyName : @"Total";
+            request.paymentSummaryItems = @[[PKPaymentSummaryItem summaryItemWithLabel:summaryLabel amount:amount]];
+
+            PKPaymentAuthorizationViewController *vc = [[PKPaymentAuthorizationViewController alloc] initWithPaymentRequest:request];
+            if (vc) {
+                vc.delegate = self;
+                UIViewController *rootViewController = [self currentViewController];
+                if (!rootViewController) {
+                    reject(@"applePay", @"Unable to present Apple Pay", nil);
+                    return;
+                }
+
+                _applePayResolve = resolve;
+                _applePayReject = reject;
+                [rootViewController presentViewController:vc animated:YES completion:nil];
+            } else {
+                reject(@"applePay", @"Apple Pay is not available", nil);
+            }
+        } @catch (NSException *exception) {
+            _applePayResolve = nil;
+            _applePayReject = nil;
+            reject(@"applePay", exception.reason ?: exception.name, nil);
+        }
+    });
+}
+
+- (UIViewController *)currentViewController {
+    UIWindow *keyWindow = UIApplication.sharedApplication.keyWindow;
+
+    if (!keyWindow) {
+        if (@available(iOS 13.0, *)) {
+            for (UIScene *scene in UIApplication.sharedApplication.connectedScenes) {
+                if (scene.activationState != UISceneActivationStateForegroundActive || ![scene isKindOfClass:[UIWindowScene class]]) {
+                    continue;
+                }
+
+                UIWindowScene *windowScene = (UIWindowScene *)scene;
+                for (UIWindow *window in windowScene.windows) {
+                    if (window.isKeyWindow) {
+                        keyWindow = window;
+                        break;
+                    }
+                }
+
+                if (keyWindow) {
+                    break;
+                }
+            }
+        }
+    }
+
+    UIViewController *viewController = keyWindow.rootViewController;
+    while (viewController.presentedViewController) {
+        viewController = viewController.presentedViewController;
+    }
+
+    return viewController;
+}
+
+- (NSString *)stringFromValue:(id)value {
+    if ([value isKindOfClass:[NSString class]]) {
+        return value;
+    }
+
+    return nil;
+}
+
+- (NSDecimalNumber *)decimalAmountFromValue:(id)value {
+    if (!value || value == [NSNull null]) {
+        return nil;
+    }
+
+    NSDecimalNumber *amount = nil;
+    if ([value isKindOfClass:[NSDecimalNumber class]]) {
+        amount = value;
+    } else if ([value isKindOfClass:[NSNumber class]]) {
+        amount = [NSDecimalNumber decimalNumberWithDecimal:[value decimalValue]];
+    } else if ([value isKindOfClass:[NSString class]]) {
+        NSString *trimmedValue = [value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        if (trimmedValue.length == 0) {
+            return nil;
+        }
+        amount = [NSDecimalNumber decimalNumberWithString:trimmedValue];
+    }
+
+    if (!amount || [amount isEqualToNumber:[NSDecimalNumber notANumber]]) {
+        return nil;
+    }
+
+    return amount;
 }
 
 - (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller
@@ -143,7 +271,7 @@ RCT_EXPORT_METHOD(applePay:(NSDictionary*)params resolver:(RCTPromiseResolveBloc
             [strongSelf sendEventWithName:@"onProgress" body:@(NO)];
             if (error) {
                 if (strongSelf.applePayResolve) {
-                    strongSelf.applePayReject(@"applePay", error.localizedDescription, error);
+                    strongSelf.applePayReject(@"applePay", error.localizedDescription ?: @"Apple Pay transaction failed", error);
                     strongSelf.applePayResolve = nil;
                     strongSelf.applePayReject = nil;
                 }
@@ -153,8 +281,10 @@ RCT_EXPORT_METHOD(applePay:(NSDictionary*)params resolver:(RCTPromiseResolveBloc
             if (strongSelf.applePayResolve) {
                 if (transaction.redirectURL) {
                     strongSelf.applePayResolve(@{@"redirectURL": transaction.redirectURL.absoluteString});
-                } else {
+                } else if (transaction.resourcePath) {
                     strongSelf.applePayResolve(@{@"resourcePath": transaction.resourcePath});
+                } else {
+                    strongSelf.applePayResolve(@{});
                 }
                 strongSelf.applePayResolve = nil;
                 strongSelf.applePayReject = nil;
@@ -164,7 +294,7 @@ RCT_EXPORT_METHOD(applePay:(NSDictionary*)params resolver:(RCTPromiseResolveBloc
     } else {
         [self sendEventWithName:@"onProgress" body:@(NO)];
         if (self.applePayResolve) {
-            self.applePayReject(@"applePay", error.localizedDescription, error);
+            self.applePayReject(@"applePay", error.localizedDescription ?: @"Invalid Apple Pay payment data", error);
             self.applePayResolve = nil;
             self.applePayReject = nil;
         }
